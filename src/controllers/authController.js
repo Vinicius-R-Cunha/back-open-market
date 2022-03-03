30502716d32e4ba1619670a74b443ffa02c4c7d2
Vinicius <vinicunha2008@gmail.com>
2022-03-02 23:23:27 -0300

    auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,28 +1,46 @@
 import bcrypt from 'bcrypt';
 import connection from "../db.js";
+import { v4 as uuid } from 'uuid';
 
 export async function signUp(req, res) {
+    try {
+        const passwordHash = bcrypt.hashSync(req.body.senha, 10);
 
-    const passwordHash = bcrypt.hashSync(req.body.senha, 10)
+        await connection.query(`
+                INSERT INTO
+                usuarios (nome,email,senha)
+                values($1,$2,$3)`
+            , [req.body.nome, req.body.email, passwordHash]);
 
-    const users = await connection.query('select * from usuarios');
-    // await connection.query('DELETE FROM usuarios');
-
-    // await connection.query(`
-    //         INSERT INTO
-    //         usuarios (nome,email,senha)
-    //         values($1,$2,$3)`
-    //     , [req.body.nome, req.body.email, passwordHash]);
-
-    // res.sendStatus(200);
-    res.status(200).send(users.rows);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
 
 export async function signIn(req, res) {
+    try {
+        const userExists = await connection.query("SELECT * FROM usuarios WHERE email=$1", [req.body.email]);
+        if (userExists.rowCount === 0) {
+            return res.sendStatus(401);
+        }
 
+        const user = userExists.rows[0];
+        if (!bcrypt.compareSync(req.body.senha, user.senha)) {
+            return res.sendStatus(401);
+        }
 
+        const token = uuid();
 
+        await connection.query(`
+            INSERT INTO
+                sessoes("idUsuario", token)
+                VALUES($1, $2)
+        `, [user.id, token]);
 
+        res.status(200).send(token);
+    } catch (error) {
 
-    res.sendStatus(200);
+        res.status(500).send(error);
+    }
 }
